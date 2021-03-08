728ddc7472685b0c8b0a5f2ee1c015ecbd693a62
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-03-08 17:27:59 +0000

    login and access control

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, flash, redirect, url_for, session,request, logging
+from flask.logging import create_logger
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt 
-
+from functools import wraps
 from data import Articles
-app = Flask(__name__)
 
+app = Flask(__name__)
+LOG = create_logger(app)
 # config mysql
 app.config['MYSQL_HOST']= 'localhost'
 app.config['MYSQL_USER']= 'root'
@@ -16,26 +18,29 @@ app.config['MYSQL_CURSORCLASS']= 'DictCursor'
 
 # init mysql
 mysql= MySQL(app)
-
-
 Articles = Articles()
-# This will create a route at /, which return the value index
+
+# Home
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# About
 @app.route('/about')
 def about ():
     return render_template('about.html')
 
+# Articles
 @app.route('/articles')
 def articles ():
     return render_template('articles.html',articles = Articles )
 
+# Single article 
 @app.route('/article/<string:id>/')
 def article (id):
     return render_template('article.html',id = id )
 
+# register form class
 class RegisterForm(Form):
     name = StringField('Name', validators=[validators.Length(min=1, max=25)])
     username  = StringField('Username', validators=[validators.Length(min=4, max=25)])
@@ -46,7 +51,7 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
-
+# user registration 
 @app.route('/register', methods=['GET', 'POST'])
 def register ():
     form = RegisterForm(request.form)
@@ -66,7 +71,70 @@ def register ():
         return redirect(url_for('index'))
     return render_template('register.html', form = form)
 
+# user login
+@app.route('/login', methods=['GET', 'POST'])
+def login ():
+    app.logger.info('-------------from login route-------------')
+
+    if request.method == "POST" :
+        username = request.form['username']
+        password_check = request.form['password']
+        # create cursor 
+        cur = mysql.connection.cursor()
+        result = cur.execute('SELECT * FROM users WHERE username =%s',[username] )
+
+        if result> 0:
+            data= cur.fetchone()
+      
+            password = data['password']
+            # compare the password
+            if sha256_crypt.verify(password_check, password):
+                session['logged_in']= True
+                session['username'] = username
+
+                flash('you are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                return render_template('login.html', error= 'Password did not match')
+        else:
+            return render_template('login.html', error= 'user not found')
+
+        # close 
+        mysql.connection.commit()
+        cur.close()
+   
+        # flash('you are now logged in', 'success')
+
+    return render_template('login.html')
+    
+# check if logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap (*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please Login', 'danger')
+            return redirect(url_for('login')) 
+    return wrap
+
+
+# dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard ():
+    return render_template('dashboard.html')
+
+# logout
+@app.route('/logout')
+def logout ():
+    session.clear()
+    flash('You have been logged out', 'success')
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.secret_key = '123123'
     app.run(debug = True)
+    
+    
