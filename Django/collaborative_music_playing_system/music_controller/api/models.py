53e7b3a0b9d156ce94f243852c002ecb0ca6ab90
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-03-10 14:58:01 +0000

    update models

--- Django/collaborative_music_playing_system/music_controller/api/models.py
+++ Django/collaborative_music_playing_system/music_controller/api/models.py
@@ -2,22 +2,21 @@ from django.db import models
 import string 
 import random
 
-def gen_unique_code():
-    length =6
+def generate_unique_code():
+    length = 6
 
-    # create a random code with length of 6 char
-    # contains only uppercase chars 
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Room.objects.filter(code = code).count() ==0:
+        if Room.objects.filter(code=code).count() == 0:
             break
+
     return code
 
 # basic rule in django is to have big models and thin views 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True )
+    code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
-    guest_can_pause = models.BooleanField(null= False, default= False)
-    votes_to_skip = models.IntegerField(null= False, default=1)
-    create_at = models.DateTimeField(auto_now_add=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
