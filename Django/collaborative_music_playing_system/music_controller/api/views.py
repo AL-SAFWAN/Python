aad0d52737c732c0a40912c8a1f4c5e3ee557237
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-03-13 19:36:36 +0000

    created a join page

--- Django/collaborative_music_playing_system/music_controller/api/views.py
+++ Django/collaborative_music_playing_system/music_controller/api/views.py
@@ -4,60 +4,88 @@ from .serializers import RoomSerializers, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your endpoints here
+from django.http import JsonResponse
+
+# Create your views here.
 
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializers
+
 
 class GetRoom(APIView):
     serializer_class = RoomSerializers
     lookup_url_kwarg = 'code'
 
-    def get(self, request, format= None):
+    def get(self, request, format=None):
         code = request.GET.get(self.lookup_url_kwarg)
-        if code !=None :
+        if code != None:
             room = Room.objects.filter(code=code)
-            if len(room)> 0:
+            if len(room) > 0:
                 data = RoomSerializers(room[0]).data
-                # to check if the user is the host
-                data['is_host']= self.request.session.session_key == room[0].host
+                data['is_host'] = self.request.session.session_key == room[0].host
                 return Response(data, status=status.HTTP_200_OK)
-            return Response({'Room Not Found': 'Invalid Room Code'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
-    # api view allows to overide the request methods
 
-    # post request for when a host is trying to create a room 
     def post(self, request, format=None):
-        # checking and creating sessions 
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
-        # create a room using the request and CreateRoomSerializer
-        serializer = self.serializer_class(data=request.data)
 
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            # If the data is correct then create the room 
             guest_can_pause = serializer.data.get('guest_can_pause')
             votes_to_skip = serializer.data.get('votes_to_skip')
-            # check if the host already created a room
             host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
-            # If it does, then update the same room
             if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializers(room).data, status=status.HTTP_200_OK)
             else:
-            # create a new room 
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
-        # .data to return data, but we could check the setting 
-        return Response(RoomSerializers(room).data, status=status.HTTP_200_OK)
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializers(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        data ={
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- Django/collaborative_music_playing_system/music_controller/frontend/src/components/RoomJoinPage.js
+++ Django/collaborative_music_playing_system/music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,71 @@
-  import React, { Component } from "react";
-
+import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from 'react-router-dom'
 export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: '',
+      error: ''
+    }
+    this.handleTextFieldChange= this.handleTextFieldChange.bind(this)
+    this.roomButtonPressed= this.roomButtonPressed.bind(this)
+
+  }
+  handleTextFieldChange(e) {
+    this.setState({
+      roomCode : e.target.value
+    })
+  }
+  roomButtonPressed(){
+    console.log(this.state)
+    const reqOpt ={
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body:JSON.stringify({
+          code: this.state.roomCode
+        })
+    }
+    fetch('/api/join-room', reqOpt).then(res => {
+      res.json()
+      if(res.ok){
+        this.props.history.push(`/room/${this.state.roomCode}`)
+      }else{
+        this.setState({error: 'Room Not Found'})
+      }
+      })
+
+  }
+  render() {
+    return <Grid container spacing={1} >
+      <Grid item xs={12} align='center'>
+        <Typography component='h4' variant='h4'>
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <TextField
+          error={this.state.error}
+          label='Code'
+          placeholder='Enter a Room Code'
+          value={this.state.roomCode}
+          helperText={this.state.error}
+          variant="outlined"
+          onChange={this.handleTextFieldChange}
+        />
+
+      </Grid>
+
+      <Grid item xs={12} align='center'>
+        <Button color='primary' variant='contained'
+         onClick={this.roomButtonPressed}
+        >Enter A Room</Button>
+      </Grid>
+      <Grid item xs={12} align='center'>
+        <Button color='secondary' variant='contained' to='/' component={Link}>Back</Button>
+      </Grid>
+    </Grid>;
   }
 
-  render() {
-    return <p>This is the room join page</p>;
-  }
 }
+
